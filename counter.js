e8db24041b8010fda77e2f7bd6b1d1b0d9d1c114
kkk12345678 <kkk12345678@users.noreply.github.com>
2022-05-05 12:09:57 +0300

    Update counter.js

--- counter.js
+++ counter.js
@@ -1,6 +1,5 @@
 let counter = 0
-document.getElementById("increment").onclick = function()
-{
+document.getElementById("increment").onclick = function() {
 	counter++
 	document.getElementById("counter").innerText = counter
 }
